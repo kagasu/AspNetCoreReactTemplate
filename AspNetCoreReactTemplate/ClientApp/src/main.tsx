b31f496f25fee49123620ab28b01fee970c6aef2
kagasu <1202244+kagasu@users.noreply.github.com>
2023-01-28 02:55:20 +0900

    Fix

--- AspNetCoreReactTemplate/ClientApp/src/main.tsx
+++ AspNetCoreReactTemplate/ClientApp/src/main.tsx
@@ -10,7 +10,5 @@ import PrimeReact from 'primereact/api'
 PrimeReact.ripple = false
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
-  </React.StrictMode>
+  <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
 )
